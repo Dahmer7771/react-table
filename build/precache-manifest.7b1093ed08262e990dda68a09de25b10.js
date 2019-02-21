1a3eaacd17f4afc32380ae47e319355fa0559cb2
Dahmer7771 <grichenko122@gmail.com>
2019-02-21 15:16:07 +0200

    build

---
+++ build/precache-manifest.7b1093ed08262e990dda68a09de25b10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8114d4e98ecaf8c9dc07",
+    "url": "/static/js/main.8114d4e9.chunk.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "/static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "8114d4e98ecaf8c9dc07",
+    "url": "/static/css/main.bfadd04c.chunk.css"
+  },
+  {
+    "revision": "5c936232b5598de4fdcdb7fe910e699c",
+    "url": "/index.html"
+  }
+];
